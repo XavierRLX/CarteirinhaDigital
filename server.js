f646d1abda6190c62e6620f7ea851c22e7f095bb
XavierRLX <renanlima2000.aer@gmail.com>
2024-06-18 14:14:44 -0300

    Reapply "add outras info"
    This reverts commit 4209c3188b29b1d211194c7eeefac90818ac6b6f.

--- server.js
+++ server.js
@@ -11,21 +11,6 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/register', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const { error } = await supabase.auth.signUp({ email, password });
-    if (error) {
-      res.status(400).json({ error: error.message });
-    } else {
-      res.status(200).json({ message: 'User registered successfully' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
