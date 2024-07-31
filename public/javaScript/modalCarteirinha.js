34ea216d6aa564106cf13f79e631e53fc5ffc215
XavierRLX <renanlima2000.aer@gmail.com>
2024-07-31 14:20:56 -0300

    feat
    removido o bloqueio da carteirinha sem localização

--- public/javaScript/modalCarteirinha.js
+++ public/javaScript/modalCarteirinha.js
@@ -1,22 +1,25 @@
 document.getElementById('acessarCarteirinha').addEventListener('click', function () {
-    const nome = userInfo.nomePerfil;
-    const dia = new Date().toLocaleDateString('pt-BR');
-    const hora = new Date().toLocaleTimeString('pt-BR');
-  
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        document.getElementById('modal').style.display = 'flex';
-        document.getElementById('menu').style.display = 'none';
-        getLocationAndSendEmail(nome, dia, hora);
-      }, error => {
-        console.error('Erro ao obter localização:', error);
-        alert('Por favor, permita o acesso à localização nas configurações do seu navegador...');
-      });
-    } else {
-      alert('Geolocalização não é suportada pelo seu navegador.');
-    }
-  });
-  
+  const nome = userInfo.nomePerfil;
+  const dia = new Date().toLocaleDateString('pt-BR');
+  const hora = new Date().toLocaleTimeString('pt-BR');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      getLocationAndSendEmail(nome, dia, hora, latitude, longitude);
+    }, error => {
+      console.error('Erro ao obter localização:', error);
+      alert('Por favor, permita o acesso à localização nas configurações do seu navegador...');
+      getLocationAndSendEmail(nome, dia, hora, 'Não informado', 'Não informado');
+    });
+  } else {
+    alert('Geolocalização não é suportada pelo seu navegador.');
+    getLocationAndSendEmail(nome, dia, hora, 'Não informado', 'Não informado');
+  }
+  document.getElementById('modal').style.display = 'flex';
+  document.getElementById('menu').style.display = 'none';
+});
 
 document.getElementById('closeModalBtn').addEventListener('click', function () {
     document.getElementById('modal').style.display = 'none';
--- public/javaScript/sendEmail.js
+++ public/javaScript/sendEmail.js
@@ -1,6 +1,6 @@
 function sendEmailUse(nome, dia, hora, addressDetails, latitude, longitude) {
   const email = "renanlima2000.aer@gmail.com";
-  console.log('Dados para envio:', { email, nome, dia, hora, addressDetails, latitude, longitude});
+  console.log('Dados para envio:', { email, nome, dia, hora, addressDetails, latitude, longitude });
 
   const { street, neighborhood, city, state, country, houseNumber } = addressDetails;
 
@@ -26,38 +26,46 @@ function sendEmailUse(nome, dia, hora, addressDetails, latitude, longitude) {
   });
 }
 
-function getLocationAndSendEmail(nome, dia, hora) {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-
-      fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
-        .then(response => response.json())
-        .then(data => {
-          console.log('Dados da localização:', data);
-          const address = data.address;
-          const addressDetails = {
-            houseNumber: address.house_number || 'Desconhecido',
-            street: address.road || 'Desconhecida',
-            neighborhood: address.suburb || address.neighborhood || 'Desconhecido',
-            city: address.city || address.town || address.village || 'Desconhecida',
-            state: address.state || 'Desconhecido',
-            country: address.country || 'Desconhecido'
-          };
-          console.log('Detalhes do Endereço:', addressDetails);
-          sendEmailUse(nome, dia, hora, addressDetails, latitude, longitude);
-        })
-        .catch(error => {
-          console.error('Erro ao obter dados da localização:', error);
-          //alert('Erro ao obter dados da localização.');
-        });
-
-    }, error => {
-      console.error('Erro ao obter localização:', error);
-    });
+function getLocationAndSendEmail(nome, dia, hora, latitude, longitude) {
+  if (latitude !== 'Não informado' && longitude !== 'Não informado') {
+    fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Dados da localização:', data);
+        const address = data.address;
+        const addressDetails = {
+          houseNumber: address.house_number || 'Desconhecido',
+          street: address.road || 'Desconhecida',
+          neighborhood: address.suburb || address.neighborhood || 'Desconhecido',
+          city: address.city || address.town || address.village || 'Desconhecida',
+          state: address.state || 'Desconhecido',
+          country: address.country || 'Desconhecido'
+        };
+        console.log('Detalhes do Endereço:', addressDetails);
+        sendEmailUse(nome, dia, hora, addressDetails, latitude, longitude);
+      })
+      .catch(error => {
+        console.error('Erro ao obter dados da localização:', error);
+        const addressDetails = {
+          houseNumber: 'Não informado',
+          street: 'Não informado',
+          neighborhood: 'Não informado',
+          city: 'Não informado',
+          state: 'Não informado',
+          country: 'Não informado'
+        };
+        sendEmailUse(nome, dia, hora, addressDetails, latitude, longitude);
+      });
   } else {
-    alert('Geolocalização não é suportada pelo seu navegador.');
+    const addressDetails = {
+      houseNumber: 'Não informado',
+      street: 'Não informado',
+      neighborhood: 'Não informado',
+      city: 'Não informado',
+      state: 'Não informado',
+      country: 'Não informado'
+    };
+    sendEmailUse(nome, dia, hora, addressDetails, latitude, longitude);
   }
 }
 
