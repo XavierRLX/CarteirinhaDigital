102ff65964f579f16db2829f170a30858cf1f820
XavierRLX <renanlima2000.aer@gmail.com>
2025-01-17 15:23:17 -0300

    Update modalCarteirinha.js
    desativado a busca pelo a loc do us

--- public/javaScript/modalCarteirinha.js
+++ public/javaScript/modalCarteirinha.js
@@ -1,29 +1,36 @@
-document.getElementById('abrirCarteirinha').addEventListener('click', function () {
-  const nome = userInfo.nomePerfil;
-  const dia = new Date().toLocaleDateString('pt-BR');
-  const hora = new Date().toLocaleTimeString('pt-BR');
+document
+  .getElementById("abrirCarteirinha")
+  .addEventListener("click", function () {
+    const nome = userInfo.nomePerfil;
+    const dia = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR");
 
-  // Função para processar a localização e enviar o email
-  const processLocation = (latitude, longitude) => {
-    getLocationAndSendEmail(nome, dia, hora, latitude, longitude);
-    document.getElementById('modal').style.display = 'flex';
-    document.getElementById('menu').style.display = 'none';
-  };
+    // Função para processar a localização e enviar o email
+    const processLocation = (latitude, longitude) => {
+      getLocationAndSendEmail(nome, dia, hora, latitude, longitude);
+      document.getElementById("modal").style.display = "flex";
+      document.getElementById("menu").style.display = "none";
+    };
 
-  // Obter localização do usuário
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      processLocation(position.coords.latitude, position.coords.longitude);
-    }, error => {
-      console.error('Erro ao obter localização:', error);
-      alert('Por favor, permita o acesso à localização nas configurações do seu navegador...');
-      processLocation('Não informado', 'Não informado');
-    });
-  } else {
-    alert('Geolocalização não é suportada pelo seu navegador.');
-    processLocation('Não informado', 'Não informado');
-  }
-});
+  //   // Obter localização do usuário
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         processLocation(position.coords.latitude, position.coords.longitude);
+  //       },
+  //       (error) => {
+  //         console.error("Erro ao obter localização:", error);
+  //         alert(
+  //           "Por favor, permita o acesso à localização nas configurações do seu navegador..."
+  //         );
+  //         processLocation("Não informado", "Não informado");
+  //       }
+  //     );
+  //   } else {
+  //     alert("Geolocalização não é suportada pelo seu navegador.");
+  //     processLocation("Não informado", "Não informado");
+  //   }
+  // });
 
 
 document.getElementById('closeModalBtn').addEventListener('click', function () {
